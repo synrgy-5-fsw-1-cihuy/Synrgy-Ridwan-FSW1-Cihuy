1720398100320cb7b035d6bf0f17b204df2ab232
ridwannhd0705 <ridwannur07@student.uns.ac.id>
2022-11-17 15:43:23 +0700

    add get by ID route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,52 @@
-const { Router } = require("express");
-const models = require("../db/models");
+const { Router } = require('express');
+const models = require('../db/models');
 const router = Router();
-const Role = models.role;
+const Role = models.Role;
 
-router.get("/", (request, response) => {
-  response.status(200).json({ data: result });
+router.post('/', async (request, response) => {
+  try {
+    // sukses
+  } catch (error) {
+    // gagal
+  }
 });
 
-router.get("/:id", async (request, response) => {
-  response.status(200).json({ data: result });
+router.get('/', async (request, response) => {
+  try {
+    const result = await Role.findAll();
+    response.status(200).json({ message: 'OK', data: result });
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ message: 'internal server error' });
+  }
 });
 
-router.post("/", (req, res) => {
-  res.send("3");
+router.get('/:id', async (request, response) => {
+  try {
+    const resultById = await Role.findByPk(request.params.id);
+    if(!resultById){
+      response.status(404).json({message: "Role not found"});
+    }
+    response.status(200).json({message: "Success", data:resultById});
+  } catch (error) {
+  response.status(500).json({message: "Internal Server Error"});
+  }
 });
 
-router.put("/:id", (req, res) => {
-  res.send("4");
+router.put('/:id', async (request, response) => {
+  try {
+    // sukses
+  } catch (error) {
+    // gagal
+  }
 });
 
-router.delete("/:id", (req, res) => {
-  res.send("5");
+router.delete('/:id', async (request, response) => {
+  try {
+    // sukses
+  } catch (error) {
+    // gagal
+  }
 });
 
 module.exports = router;
